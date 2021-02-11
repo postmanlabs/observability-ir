a548c32e7fbe0562e99b7da4aceed8bd2998adca
Kevin Ku <kevin@akitasoftware.com>
2021-02-11 15:55:51 -0800

    Add generated python code.

--- gen.sh
+++ gen.sh
@@ -9,10 +9,12 @@ pushd ${SCRIPTPATH} > /dev/null
 for d in proto/*; do
   pkg=$(basename ${d})
   mkdir -p go/${pkg}
+  mkdir -p py/${pkg}
 
   # Use absolute path because docker volume likes that.
   proto_dir=$(cd ${d}; pwd)
   go_out=$(cd go/${pkg}; pwd)
+  py_out=$(cd py/${pkg}; pwd)
 
   # Using namely/protoc:1.26_1 (libprotoc 3.8.0) to output go code using
   # golang protobuf APIv1.
@@ -23,6 +25,14 @@ for d in proto/*; do
     --entrypoint /bin/sh \
     namely/protoc:1.26_1 \
     -c '/usr/local/bin/protoc -I /opt/include -I /defs --go_out paths=source_relative:/go_out /defs/*.proto'
+
+  docker run --rm \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    -v ${proto_dir}:/defs \
+    -v ${py_out}:/py_out \
+    --entrypoint /bin/sh \
+    namely/protoc:1.26_1 \
+    -c '/usr/local/bin/protoc -I /opt/include -I /defs --python_out /py_out /defs/*.proto'
 done
 
 popd > /dev/null
---
+++ py/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="api_spec",
+    version="0.0.1",
+    author="Akita Software Inc",
+    author_email="info@akitasoftware.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
